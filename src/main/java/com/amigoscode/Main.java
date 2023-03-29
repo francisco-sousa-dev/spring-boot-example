636303ba52ee689884fb728f3b5ab7efae1e9a00
Francisco Sousa <franciscosousadev@gmail.com>
2023-03-29 16:53:33 -0300

    annotation PathVariable

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -3,6 +3,7 @@ package com.amigoscode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -33,6 +34,17 @@ public class Main {
     @GetMapping("api/v1/customers")
     public List<Customer> getCustomers() {
         return customers;
+    }
+
+    @GetMapping("api/v1/customers/{customerId}")
+    public Customer getCustomer(@PathVariable("customerId") Integer customerId) {
+        Customer customer = customers
+                .stream()
+                .filter(c -> c.id.equals(customerId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("customer with id [%s] not found".formatted(customerId)));
+
+        return customer;
     }
 
     static class Customer {
